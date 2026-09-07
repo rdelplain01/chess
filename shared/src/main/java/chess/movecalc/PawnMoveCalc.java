30e970f0900a00719cd571bf987dd1200c33a8a3
Richard <richard@delplain.com>
2026-09-07 11:40:27 -0600

    White Pawn everything

--- shared/src/main/java/chess/movecalc/PawnMoveCalc.java
+++ shared/src/main/java/chess/movecalc/PawnMoveCalc.java
@@ -12,17 +12,16 @@ public class PawnMoveCalc {
     public Collection<ChessMove> calculate(ChessPosition myPosition, ChessBoard board, ChessGame.TeamColor pieceColor) {
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
-        ChessMove move;
-//        int mutRow;
-//        int mutCol;
-        int[][] moveSet;
         List<ChessMove> moves = new ArrayList<>();
 
         if (row == 2) {
-            moves.add(new ChessMove(
-                new ChessPosition(row, col),
-                new ChessPosition(row + 2, col),
-                null));
+            ChessPiece endPiece = board.getPiece(new ChessPosition(row+2, col));
+            if (endPiece == null) {
+                moves.add(new ChessMove(
+                        new ChessPosition(row, col),
+                        new ChessPosition(row + 2, col),
+                        null));
+            }
         }
 
         ChessPiece blockPiece = board.getPiece(new ChessPosition(row+1, col));
@@ -32,6 +31,18 @@ public class PawnMoveCalc {
                         new ChessPosition(row, col),
                         new ChessPosition(row + 1, col),
                         ChessPiece.PieceType.QUEEN));
+                moves.add(new ChessMove(
+                        new ChessPosition(row, col),
+                        new ChessPosition(row + 1, col),
+                        ChessPiece.PieceType.KNIGHT));
+                moves.add(new ChessMove(
+                        new ChessPosition(row, col),
+                        new ChessPosition(row + 1, col),
+                        ChessPiece.PieceType.BISHOP));
+                moves.add(new ChessMove(
+                        new ChessPosition(row, col),
+                        new ChessPosition(row + 1, col),
+                        ChessPiece.PieceType.ROOK));
             } else {
                 moves.add(new ChessMove(
                         new ChessPosition(row, col),
@@ -48,6 +59,27 @@ public class PawnMoveCalc {
         for (int[] ints : captureSet) {
             int mutRow = row + ints[0];
             int mutCol = col + ints[1];
+            if (mutRow == 8) {
+                ChessPiece endPiece = board.getPiece(new ChessPosition(mutRow, mutCol));
+                if (endPiece != null && endPiece.getTeamColor() != ChessGame.TeamColor.WHITE) {
+                    moves.add(new ChessMove(
+                            new ChessPosition(row, col),
+                            new ChessPosition(mutRow, mutCol),
+                            ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(
+                            new ChessPosition(row, col),
+                            new ChessPosition(mutRow, mutCol),
+                            ChessPiece.PieceType.KNIGHT));
+                    moves.add(new ChessMove(
+                            new ChessPosition(row, col),
+                            new ChessPosition(mutRow, mutCol),
+                            ChessPiece.PieceType.BISHOP));
+                    moves.add(new ChessMove(
+                            new ChessPosition(row, col),
+                            new ChessPosition(mutRow, mutCol),
+                            ChessPiece.PieceType.ROOK));
+                }
+            }
             if(mutRow > 7 || mutCol > 7) {
                 continue;
             }
@@ -59,110 +91,6 @@ public class PawnMoveCalc {
                         null));
             }
         }
-
-//        if (pieceColor == ChessGame.TeamColor.WHITE) {
-//            moveSet = new int[][]{
-//                    { 1, 0},
-//                    { 1, 1},
-//                    { 1,-1}
-//            };
-//
-//        } else {
-//            moveSet = new int[][]{
-//                    {-1, 0},
-//                    {-1, 1},
-//                    {-1,-1}
-//            };
-//        }
-//
-//        for (int[] ints : moveSet) {
-//            int mutRow = row + ints[0];
-//            int mutCol = col + ints[1];
-//        }
-
-//        // Normal Move up
-//        if (!(mutRow < 2 || mutRow > 7 || mutCol < 1 || mutCol > 8)) {
-//            move = new ChessMove(
-//                    new ChessPosition(row, col),
-//                    new ChessPosition(mutRow, mutCol),
-//                    null);
-//            ChessPiece endPiece = board.getPiece(move.getEndPosition());
-//            if (endPiece == null) {
-//                moves.add(move);
-//            }
-//        }
-//
-//        //Premotion
-//        if (mutRow == 1 || mutRow == 8) {
-//            move = new ChessMove(
-//                    new ChessPosition(row, col),
-//                    new ChessPosition(mutRow, mutCol),
-//                    ChessPiece.PieceType.QUEEN);
-//            ChessPiece endPiece = board.getPiece(move.getEndPosition());
-//            if (endPiece == null) {
-//                moves.add(move);
-//            }
-//        }
-//
-//        //Capture Diagnal
-//        mutCol = col + 1;
-//        // Normal Move up
-//        if (!(mutRow < 2 || mutRow > 7 || mutCol < 1 || mutCol > 8)) {
-//            move = new ChessMove(
-//                    new ChessPosition(row, col),
-//                    new ChessPosition(mutRow, mutCol),
-//                    null);
-//            ChessPiece endPiece = board.getPiece(move.getEndPosition());
-//            if (endPiece != null) {
-//                moves.add(move);
-//            }
-//        }
-//
-//        //Premotion
-//        if (mutRow == 1 || mutRow == 8) {
-//            move = new ChessMove(
-//                    new ChessPosition(row, col),
-//                    new ChessPosition(mutRow, mutCol),
-//                    ChessPiece.PieceType.QUEEN);
-//            ChessPiece endPiece = board.getPiece(move.getEndPosition());
-//            if (endPiece != null) {
-//                moves.add(move);
-//            }
-//        }
-//
-//        //Capture Diagnal
-//        mutCol = col - 1;
-//        // Normal Move up
-//        if (!(mutRow < 2 || mutRow > 7 || mutCol < 1 || mutCol > 8)) {
-//            move = new ChessMove(
-//                    new ChessPosition(row, col),
-//                    new ChessPosition(mutRow, mutCol),
-//                    null);
-//            ChessPiece endPiece = board.getPiece(move.getEndPosition());
-//            if (endPiece != null) {
-//                moves.add(move);
-//            }
-//        }
-//
-//        //Premotion
-//        if (mutRow == 1 || mutRow == 8) {
-//            move = new ChessMove(
-//                    new ChessPosition(row, col),
-//                    new ChessPosition(mutRow, mutCol),
-//                    ChessPiece.PieceType.QUEEN);
-//            ChessPiece endPiece = board.getPiece(move.getEndPosition());
-//            if (endPiece != null) {
-//                moves.add(move);
-//            }
-//        }
-//
-//
-//
-//
-//        move = new ChessMove(
-//                new ChessPosition(row, col),
-//                new ChessPosition(mutRow, mutCol),
-//                null);
 
         return moves;
     }
